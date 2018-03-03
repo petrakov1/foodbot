1471e014a75dc415794934b2ff5327f7a4048608
Tema Ustyukhin <temes_matematik@mail.ru>
2018-03-04 01:39:41 +0300

    returnJson

--- dataAnal.py
+++ dataAnal.py
@@ -36,26 +36,29 @@ def sortByValue(arr):
 #arrPlaces = [{"a":30,"b":4,"d":10},{"a":1,"b":2,"c":3},{"a":3,"b":4,"c":5},]
 
 #DATA SET
-arrPerson = {"burger":1,"pizza":2,"c":3,"d":4}
-personPrise = 2
+#arrPerson = {"burger":1,"pizza":2,"c":3,"d":4}
+#personPrise = 2
 
-def getTopPlaces (json):
+def getTopPlaces (json_Plase, json_Person):
+
+    arrPerson = json_Person["tags"]
+    personPrise = json_Person["price"]
 
     arrPlaces = []
-    for place in json:
+    for place in json_Plase:
         arrPlaces.append([place["id"], valueByTag(place["tags"], arrPerson)])
 
-    print (arrPlaces)
+    #print (arrPlaces)
     sortByValue(arrPlaces)
-    print ("After sort")
-    print (arrPlaces)
+    #print ("After sort")
+    #print (arrPlaces)
     for i in range(1, arrPlaces.__len__()):
         arrPlaces[i][1] = arrPlaces[i][1] / float(arrPlaces[0][1])
     arrPlaces[0][1] = 1
-    print ("After 0-1")
-    print (arrPlaces)
+    #print ("After 0-1")
+    #print (arrPlaces)
 
-    print("test")
+    #print("test")
 
     # for tegs
     # max5 = 0
@@ -78,32 +81,33 @@ def getTopPlaces (json):
     # arrValueOfPrices = [[1,2,3],[2,1,3]]
     arrValueOfPrices = []
 
-    for place in json:
+    for place in json_Plase:
         arrValueOfPrices.append([place["id"], place["price"]])
-    print (arrValueOfPrices)
+    #print (arrValueOfPrices)
 
     for i in range(0, arrValueOfPrices.__len__()):
         arrValueOfPrices[i][1] = (personPrise - arrValueOfPrices[i][1]) / 2.0
 
     sortByValue(arrValueOfPrices)
-    print ("Before connection")
-    print (arrValueOfPrices)
-    print (arrPlaces)
+    #print ("Before connection")
+    #print (arrValueOfPrices)
+    #print (arrPlaces)
 
     for i in range(0, arrPlaces.__len__()):
         for j in range(0, arrValueOfPrices.__len__()):
             if (arrPlaces[i][0]==arrValueOfPrices[j][0]):
                 arrPlaces[i][1] += arrValueOfPrices[j][1]
 
-    print ("Itog")
+    #print ("Itog")
     sortByValue(arrPlaces)
-    print (arrPlaces)
-
+    #print (arrPlaces)
+    return (arrPlaces)
 
 #json = [{"name": "test1", "tags": {"burger": 1, "pizza": 2}, "price": 1, "location": "", "id": 1, "desc": "@"},
 #                      {"name": "test2", "tags": {"pizza": 1}, "price": 2, "location": "", "id": 2, "desc": "@"},
 #            {"name": "v", "tags": {"pasta": 2, "burger": 1, "pizza": 1}, "price": 3, "location": {}, "id": 6, "desc": ""}]
 
-#json = [{"name": "\u0422\u0435\u0441\u0442", "tags": {"burger": 1}, "price": 1, "location": "", "id": 1, "desc": "@"}, {"name": "\u0422\u0435\u0441\u0442", "tags": {"burger": 1}, "price": 1, "location": "", "id": 2, "desc": "@"}, {"name": "\u0422\u0435\u0441\u0442", "tags": {"burger": 1}, "price": 1, "location": "", "id": 3, "desc": "@"}, {"name": "Pizaa", "tags": {"pasta": 1}, "price": 1, "location": {}, "id": 4, "desc": ""}, {"name": "a", "tags": {"pasta": 1, "fri": 1}, "price": 2, "location": {}, "id": 5, "desc": ""}, {"name": "Pizaa", "tags": {"fri": 2}, "price": 1, "location": {}, "id": 6, "desc": ""}, {"name": "a", "tags": {"burger": 1, "fri": 1}, "price": 2, "location": {}, "id": 7, "desc": ""}]
+#jsonPlaces = [{"name": "\u0422\u0435\u0441\u0442", "tags": {"burger": 1}, "price": 1, "location": "", "id": 1, "desc": "@"}, {"name": "\u0422\u0435\u0441\u0442", "tags": {"burger": 1}, "price": 1, "location": "", "id": 2, "desc": "@"}, {"name": "\u0422\u0435\u0441\u0442", "tags": {"burger": 1}, "price": 1, "location": "", "id": 3, "desc": "@"}, {"name": "Pizaa", "tags": {"pasta": 1}, "price": 1, "location": {}, "id": 4, "desc": ""}, {"name": "a", "tags": {"pasta": 1, "fri": 1}, "price": 2, "location": {}, "id": 5, "desc": ""}, {"name": "Pizaa", "tags": {"fri": 2}, "price": 1, "location": {}, "id": 6, "desc": ""}, {"name": "a", "tags": {"burger": 1, "fri": 1}, "price": 2, "location": {}, "id": 7, "desc": ""}]
+#jsonPerson = {"tags": {"burger":1,"pizza":2}, "price": 1, "places": 0}
 
-#getTopPlaces(json)
+#getTopPlaces(jsonPlaces,jsonPerson)
